4be2e3023b5861648ee8c62b2cb8373817a3399e
Michael Feinberg <mfeinberg32999@gmail.com>
2017-02-14 10:49:43 -0500

    Octant 2 done

--- draw.c
+++ draw.c
@@ -7,18 +7,52 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  int B = x0 - x1;
-  int A = y1 - y0;
-  int d = B/2 + A;
-  int y = y0;
-  int x = x0;
-  while (x <= x1) {
-    plot(s,c,x,y);
-    if(d > 0){
-      d += B;
+  if(x1 > x0){
+    int B = x0 - x1;
+    int A = y1 - y0;
+    int dx = -B;
+  }else{
+    int B = x1 - x0;
+    int A = y0 - y1;
+    int dx = -B;
+  }
+  int dy = A;
+  //OCTANT I
+  if(dy > 0 && dx > dy){
+    int d = B+2A;
+    int y = y0;
+    int x = x0;
+    while (x <= x1) {
+      plot(s,c,x,y);
+      if(d > 0){
+	d += 2B;
+	y++;
+      }
+      d += 2A;
+      x++;
+    }
+  }
+  //OCTANT II
+  else if(dy > 0 && dy > dx){
+    int d = B+2A;
+    int y = y0;
+    int x = x0;
+    while (y <= y1) {
+      plot(s,c,x,y);
+      if(d < 0){
+	d += 2A;
+	x++;
+      }
+      d += 2B;
       y++;
     }
-    d += A;
-    x++;
+  }
+  //OCTANT VII
+  else if(dy < 0 && (-dy) > dx){
+    
+  }
+  //OCTANT VIII
+  else{
+
   }
 }
