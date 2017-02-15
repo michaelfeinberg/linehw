6ee9ecf50f383adb198e43ecb207ea68b0594673
Michael Feinberg <mfeinberg32999@gmail.com>
2017-02-15 10:59:05 -0500

    all octants complete

--- draw.c
+++ draw.c
@@ -7,52 +7,74 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  if(x1 > x0){
-    int B = x0 - x1;
-    int A = y1 - y0;
-    int dx = -B;
-  }else{
-    int B = x1 - x0;
-    int A = y0 - y1;
-    int dx = -B;
+  int B, A, dx, dy;
+  if(x1 < x0){
+    draw_line(x1,y1,x0,y0,s,c);
   }
-  int dy = A;
+  B = x0 - x1;
+  A = y1 - y0;
+  dx = -B;
+  dy = A;
   //OCTANT I
-  if(dy > 0 && dx > dy){
-    int d = B+2A;
+  if(dy > 0 && dx >= dy){
+    int d = B+2*A;
     int y = y0;
     int x = x0;
     while (x <= x1) {
       plot(s,c,x,y);
       if(d > 0){
-	d += 2B;
+	d += 2*B;
 	y++;
       }
-      d += 2A;
+      d += 2*A;
       x++;
     }
   }
   //OCTANT II
   else if(dy > 0 && dy > dx){
-    int d = B+2A;
+    int d = B+2*A;
     int y = y0;
     int x = x0;
     while (y <= y1) {
       plot(s,c,x,y);
       if(d < 0){
-	d += 2A;
+	d += 2*A;
 	x++;
       }
-      d += 2B;
+      d += 2*B;
       y++;
     }
   }
-  //OCTANT VII
-  else if(dy < 0 && (-dy) > dx){
-    
-  }
   //OCTANT VIII
+  else if(dy < 0 && (-dy) >= dx){
+    int d = 2*A-B;
+    int y = y0;
+    int x = x0;
+    while (y >= y1) {
+      plot(s,c,x,y);
+      if(d > 0){
+        d += 2*A;
+        x++;
+      }
+      d -= 2*B;
+      y--;
+    }
+
+  }
+  //OCTANT VII
   else{
+    int d = A-2*B;
+    int y = y0;
+    int x = x0;
+    while (x <= x1) {
+      plot(s,c,x,y);
+      if(d < 0){
+        d -= 2*B;
+        y--;
+      }
+      d += 2*A;
+      x++;
+    }
 
   }
 }
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
+#include <stdio.h>
+
+#include <string.h>
 #include "ml6.h"
 #include "display.h"
 #include "draw.h"
@@ -10,19 +10,41 @@ int main() {
 
   screen s;
   color c;
- 
+  int x0,y0,x1,y1;
   
   c.red = 0;
   c.green = MAX_COLOR;
   c.blue = 0;
   
   clear_screen(s);
-  int x0 = 100;
-  int y0 = 100;
-  int x1 = 200;
-  int y1 = 200;
-  draw_line(x0,y0,x1,y1,s,c);
-  
+  int i = 0;
+  int j = 0;
+  while(i < 5){
+    while(j < 5){
+      x0 = i*100;
+      y0 = j*100;
+      c.red = x0;
+      c.green = y0;
+      c.blue = y0 - x0;
+      
+      x1 = 0;
+      y1 = 250;
+      while(y1 < 500){
+	draw_line(x0,y0,x1,y1,s,c);
+	x1+=5;
+	y1+=5;
+      }
+      x1 = 0;
+      y1 = 250;
+      while(y1 < 500){
+        draw_line(x0,y0,x1,y1,s,c);
+	x1+=5;
+	y1-=5;
+      }
+      j++;
+    }
+    i++;
+  }
   display(s);
   save_extension(s, "lines.png");
 }  
